25f9aee4e4b339cdf1f095e880023b7a4a33ee6b
tuon1602 <tuan69878@gmail.com>
2024-06-04 07:05:38 +0700

    ver 1.0

--- src/app/(auth)/login/_components/LoginForm.tsx
+++ src/app/(auth)/login/_components/LoginForm.tsx
@@ -1,9 +1,11 @@
 "use client"
 import React from 'react'
+import { cn } from '@/lib/utils'
+import { styles } from '@/app/theme/common'
 
 const LoginForm = () => {
   return (
-    <div className=''>LoginForm</div>
+    <div className={cn(styles.heading , "container text-center")}>LoginForm</div>
   )
 }
 
---
+++ src/app/theme/common.ts
@@ -0,0 +1,4 @@
+export const styles = {
+     heading:"text-5xl text-balance font-extrabold tracking-tight"
+}
+   
