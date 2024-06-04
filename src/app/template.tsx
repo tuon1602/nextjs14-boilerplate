85e01d7215d47b6424796c667ecef6984f0c445c
tuon1602 <tuan69878@gmail.com>
2024-06-04 23:45:41 +0700

    ver 1.0 add transition

---
+++ src/app/template.tsx
@@ -0,0 +1,5 @@
+
+import Transition from "@/components/Transition"
+export default function Template({ children }: { children: React.ReactNode }) {
+    return <Transition>{children}</Transition>
+  }
