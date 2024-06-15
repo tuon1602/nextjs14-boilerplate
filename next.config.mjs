fd96bd66e43befb6f84350dc0c6eeae34eaad5e0
tuon1602 <tuan69878@gmail.com>
2024-06-15 14:04:58 +0700

    ver 1.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
+import createNextIntlPlugin from "next-intl/plugin";
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
+// stales time for prefetching server side data
 const nextConfig = {
-    output:"standalone"
+  output: "standalone",
+  experimental: {
+    staleTimes: {
+      dynamic: 5,
+      static: 300,
+    },
+  },
 };
- 
+
 export default withNextIntl(nextConfig);
