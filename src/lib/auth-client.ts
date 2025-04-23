2a672e6b5a989f121d34d86edabf263ad8196e20
tuon1602 <tuan69878@gmail.com>
2025-04-23 17:05:23 +0700

    fix: fix auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 import Config from "../../config";
-export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: Config.BETTERAUTH.BASEURL,
-});
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const { signIn, signUp, useSession,signOut } = createAuthClient({
+    baseURL: Config.BETTERAUTH.BASEURL,
+})
