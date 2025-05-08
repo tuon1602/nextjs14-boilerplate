fc1b008ea9b12c6bafa8b6b34036b5e472e1b2bc
tuon1602 <tuan69878@gmail.com>
2025-05-08 14:16:25 +0700

    fix:delete better auth

--- src/lib/auth-client.ts
+++
@@ -1,6 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-import Config from "../../config";
-
-export const { signIn, signUp, useSession,signOut } = createAuthClient({
-    baseURL: Config.BETTERAUTH.BASEURL,
-})
